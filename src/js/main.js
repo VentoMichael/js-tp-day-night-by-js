f07ef28111e8eafdd49734900e0be13080d8d69d
Vento <Michael.vento@student.hepl.be>
2019-11-27 14:56:24 +0100

    Final commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+document.documentElement.classList.add('js-enabled');
+
+let toggleDN = document.querySelector('.tumbler__wrapper');
+let post = document.querySelectorAll('.post');
+let tumbler = document.querySelector('.tumbler');
+
+function toggleFn(event) {
+    if (toggleDN) {
+        document.body.classList.toggle('body--night-mode');
+        tumbler.classList.toggle('tumbler--night-mode');
+        for (let i = 0; i < post.length; i++) {
+            post[i].classList.toggle('post--night-mode');
+        }
+    }
+}
+
+
+toggleDN.addEventListener('click', toggleFn);
